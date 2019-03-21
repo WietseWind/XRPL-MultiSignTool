957ed24f941cc78621f75634022f27cca6129276
Wietse Wind <mail@wietse.com>
2019-03-21 16:51:01 +0100

    Remove SweetAlert (nothing to Alert yet)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import SweetAlert from 'sweetalert'
+// import SweetAlert from 'sweetalert'
 import RippledEnv from './env'
 import VueClipboard from 'vue-clipboard2'
 
 VueClipboard.config.autoSetContainer = true
 Vue.use(VueClipboard)
 
-Vue.prototype.$swal = SweetAlert
+// Vue.prototype.$swal = SweetAlert
 Vue.prototype.$env = RippledEnv
 
 Vue.config.productionTip = false
